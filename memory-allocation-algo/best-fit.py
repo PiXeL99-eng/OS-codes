6f72b035c4b44def9dbac7a40fb1fe2ed520ac92
Sayantan Kundu <sayantankundu02@gmail.com>
2023-11-22 21:04:11 +0530

    added best fit and worst fit solution

---
+++ memory-allocation-algo/best-fit.py
@@ -0,0 +1,91 @@
+class Partition:
+    # The memory partition class
+    def __init__(self, size):
+        self.size = size
+        self.next = None
+
+head = Partition(300)
+tail = head
+
+node = Partition(600)
+tail.next = node
+tail = tail.next
+
+node = Partition(350)
+tail.next = node
+tail = tail.next
+
+node = Partition(200)
+tail.next = node
+tail = tail.next
+
+node = Partition(750)
+tail.next = node
+tail = tail.next
+
+node = Partition(125)
+tail.next = node
+tail = tail.next
+
+def best_fit(node, process_size):
+
+    target = None
+
+    while node:
+        if node.size >= process_size:
+
+            if target == None:
+                target = node
+            elif target.size > node.size:
+                target = node
+
+        node = node.next
+        
+    if target == None:
+        return False
+    
+    target.size -= process_size
+    return True
+
+def print_partitions(node):
+
+    while node:
+
+        print(node.size, 'KB', end='')
+
+        if node.next != None:
+            print(' -> ', end='')
+
+        node = node.next
+
+    print()
+
+
+processes = [115, 500, 358, 200, 375]   # each of these are processes with size in KB
+
+print('Incoming processes are:')
+
+for i in processes:
+    print(i, 'KB, ', end='')
+
+print('\n')
+
+print('Inital free memory partitions:')
+print_partitions(head)
+
+for i in range(len(processes)):
+
+    print('-------------------------------------------------------------')
+
+    print(f'Allocating memory for a {processes[i]} KB process')
+    check = best_fit(head, processes[i])
+
+    if check:
+        print('✅ Successfully allocated memory to the process')
+    else:
+        print('❌ Could not allocate memory to the process')
+
+    print('Memory partitions available are:')
+    print_partitions(head)
+    
+
